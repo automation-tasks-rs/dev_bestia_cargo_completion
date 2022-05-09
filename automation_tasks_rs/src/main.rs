41c9001ea30e997d7a5fad1a5a2f6744ad4d0abb
Luciano <luciano.bestia@gmail.com>
2022-05-09 11:23:39 +0200

    author

--- automation_tasks_rs/src/main.rs
+++ automation_tasks_rs/src/main.rs
@@ -1,8 +1,8 @@
-//! automation_tasks_rs with_lib
+//! automation_tasks_rs for dev_bestia_cargo_completion
 
 use cargo_auto_lib::*;
 
-/// automation_tasks_rs with_lib
+/// automation_tasks_rs for dev_bestia_cargo_completion
 fn main() {
     exit_if_not_run_in_rust_project_root_directory();
 
@@ -69,7 +69,7 @@ fn completion() {
     /*
     // the second level if needed
     else if last_word == "new" {
-        let sub_commands = vec!["with_lib"];
+        let sub_commands = vec!["x"];
         completion_return_one_or_more_sub_commands(sub_commands, word_being_completed);
     }
     */
